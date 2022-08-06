5d7a841a9396c3eebb674b380b0193ed866c60a6
shino <1471386835@qq.com>
2022-08-06 09:21:36 +0800

    配置vercel代理

---
+++ api/proxy.js
@@ -0,0 +1,25 @@
+const request = require('request');
+
+module.exports = (req, res) => {
+    // proxy middleware options
+    let prefix = "/notion-api"
+    if (!req.url.startsWith(prefix)) {
+        return;
+    }
+    let target = "https://manager-server.yyshino.top/" + req.url.substring(prefix.length);
+
+    var options = {
+        'method': 'GET',
+        'url': target,
+        'headers': {
+            'Notion-Version': res.headers['notion-version'],
+            'Authorization': res.headers['authorization']
+        }
+    };
+    request(options, function (error, response) {
+        if (error) throw new Error(error);
+        res.writeHead(200, { "Content-Type": "application/json" });
+        res.write(response.body);
+        res.end();
+    });
+}
