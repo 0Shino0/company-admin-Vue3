45b1da4978122be3cb4ede1bde7bcbf041887e5e
shino <1471386835@qq.com>
2022-08-06 11:47:37 +0800

    路径更改

--- src/config/index.js
+++ src/config/index.js
@@ -4,9 +4,9 @@
 const env = import.meta.env.MODE || 'prod';
 const EnvConfig = {
     development: {
-        baseApi: '/backend',
+        // baseApi: '/backend',
         // baseApi: 'https://company-admin-server-production.up.railway.app/api',
-        // baseApi: 'https://manager-server.yyshino.top/api/',
+        baseApi: 'https://manager-server.yyshino.top/api/',
         mockApi: 'https://www.fastmock.site/mock/bdc39fe45bc0f9e22ae8dec88aeff8fe/api'
     },
     test: {
