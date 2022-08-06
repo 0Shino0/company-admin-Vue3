730b48a4e7a7aa84b90bb5528ce8319e3faaefaa
shino <1471386835@qq.com>
2022-08-06 10:02:27 +0800

    baseapi

--- api/proxy.js
+++ api/proxy.js
@@ -13,10 +13,10 @@ export default (req, res) => {
     createProxyMiddleware({
         target,
         changeOrigin: true,
-        pathRewrite: {
-            // 通过路径重写，去除请求路径中的 `/backend`
-            // 例如 /backend/user/login 将被转发到 http://backend-api.com/user/login
-            '^/backend/': '/'
-        }
+        // pathRewrite: {
+        //     // 通过路径重写，去除请求路径中的 `/backend`
+        //     // 例如 /backend/user/login 将被转发到 http://backend-api.com/user/login
+        //     '^/backend/': '/'
+        // }
     })(req, res)
 }
