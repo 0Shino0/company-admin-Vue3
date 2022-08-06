8fa0ea43704011611aca151343d5e1a542d45a68
shino <1471386835@qq.com>
2022-08-06 09:34:59 +0800

    路径改写

--- src/config/index.js
+++ src/config/index.js
@@ -4,9 +4,9 @@
 const env = import.meta.env.MODE || 'prod';
 const EnvConfig = {
     development: {
-        // baseApi: '/api/',
+        baseApi: '/backend/',
         // baseApi: 'https://company-admin-server-production.up.railway.app/api',
-        baseApi: 'https://manager-server.yyshino.top/api/',
+        // baseApi: 'https://manager-server.yyshino.top/api/',
         mockApi: 'https://www.fastmock.site/mock/bdc39fe45bc0f9e22ae8dec88aeff8fe/api'
     },
     test: {
@@ -14,8 +14,8 @@ const EnvConfig = {
         mockApi: 'https://www.fastmock.site/mock/bdc39fe45bc0f9e22ae8dec88aeff8fe/api'
     },
     prod: {
-        // baseApi: '/api/',
-        baseApi: 'https://manager-server.yyshino.top/api/',
+        baseApi: '/backend/',
+        // baseApi: 'https://manager-server.yyshino.top/api/',
         mockApi: 'https://www.fastmock.site/mock/bdc39fe45bc0f9e22ae8dec88aeff8fe/api'
     }
 }
